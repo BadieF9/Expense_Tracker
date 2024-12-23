4b4460218ca0eaf12c61879b2a9c9856f2ebb564
Badie <badie.farzandi@gmail.com>
2024-12-23 12:13:15 +0330

    category and task management screens updated now they work using AddCategoryDialog and AddTagDialog

--- lib/category_management_screen.dart
+++ lib/category_management_screen.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracking_app/dialogs/add_category_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../models/category.dart';
@@ -8,76 +9,45 @@ class CategoryManagementScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ExpenseProvider>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Manage Categories'),
       ),
-      body: Consumer<ExpenseProvider>(
-        builder: (context, provider, child) {
-          return ListView.builder(
-            itemCount: provider.categories.length,
-            itemBuilder: (context, index) {
-              final category = provider.categories[index];
-              return ListTile(
-                title: Text(category.name),
-                trailing: IconButton(
-                  icon: const Icon(
-                    Icons.delete,
-                    color: Colors.red,
-                  ),
-                  onPressed: () {
-                    provider.removeCategory(category.id);
-                  },
-                ),
-              );
-            },
+      body: ListView.builder(
+        itemCount: provider.categories.length,
+        itemBuilder: (context, index) {
+          final category = provider.categories[index];
+          return ListTile(
+            title: Text(category.name),
+            trailing: IconButton(
+              icon: const Icon(
+                Icons.delete,
+                color: Colors.red,
+              ),
+              onPressed: () {
+                provider.removeCategory(category.id);
+              },
+            ),
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _showAddCategoryDialog(context);
+        onPressed: () async {
+          String? categoryName = await showDialog(
+              context: context,
+              builder: (context) => const AddCategoryDialog());
+          if (categoryName != null) {
+            final category = Category(
+              id: DateTime.now().millisecondsSinceEpoch.toString(),
+              name: categoryName,
+            );
+            provider.addCategory(category);
+          }
         },
         tooltip: 'Add Category',
         child: const Icon(Icons.add),
       ),
-    );
-  }
-
-  void _showAddCategoryDialog(BuildContext context) {
-    final TextEditingController categoryNameController =
-        TextEditingController();
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Add Category'),
-          content: TextField(
-            controller: categoryNameController,
-            decoration: const InputDecoration(labelText: 'Category Name'),
-          ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                final category = Category(
-                  id: DateTime.now().millisecondsSinceEpoch.toString(),
-                  name: categoryNameController.text,
-                );
-                Provider.of<ExpenseProvider>(context, listen: false)
-                    .addCategory(category);
-                Navigator.pop(context);
-              },
-              child: const Text('Add'),
-            ),
-          ],
-        );
-      },
     );
   }
 }
--- lib/tag_management_screen.dart
+++ lib/tag_management_screen.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracking_app/dialogs/add_tag_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:expense_tracking_app/models/tag.dart';
@@ -8,75 +9,44 @@ class TagManagementScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ExpenseProvider>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Manage Tags'),
       ),
-      body: Consumer<ExpenseProvider>(
-        builder: (context, provider, child) {
-          return ListView.builder(
-            itemCount: provider.tags.length,
-            itemBuilder: (context, index) {
-              final tag = provider.tags[index];
-              return ListTile(
-                title: Text(tag.name),
-                trailing: IconButton(
-                  icon: const Icon(
-                    Icons.delete,
-                    color: Colors.red,
-                  ),
-                  onPressed: () {
-                    provider.removeTag(tag.id);
-                  },
-                ),
-              );
-            },
+      body: ListView.builder(
+        itemCount: provider.tags.length,
+        itemBuilder: (context, index) {
+          final tag = provider.tags[index];
+          return ListTile(
+            title: Text(tag.name),
+            trailing: IconButton(
+              icon: const Icon(
+                Icons.delete,
+                color: Colors.red,
+              ),
+              onPressed: () {
+                provider.removeTag(tag.id);
+              },
+            ),
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _showAddTagDialog(context);
+        onPressed: () async {
+          String? tagName = await showDialog(
+              context: context, builder: (context) => const AddTagDialog());
+          if (tagName != null) {
+            final tag = Tag(
+              id: DateTime.now().millisecondsSinceEpoch.toString(),
+              name: tagName,
+            );
+            provider.addTag(tag);
+          }
         },
         tooltip: 'Add Tag',
         child: const Icon(Icons.add),
       ),
-    );
-  }
-
-  void _showAddTagDialog(BuildContext context) {
-    final TextEditingController tagNameController = TextEditingController();
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Add Tag'),
-          content: TextField(
-            controller: tagNameController,
-            decoration: const InputDecoration(labelText: 'Tag Name'),
-          ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                final tag = Tag(
-                  id: DateTime.now().millisecondsSinceEpoch.toString(),
-                  name: tagNameController.text,
-                );
-                Provider.of<ExpenseProvider>(context, listen: false)
-                    .addTag(tag);
-                Navigator.pop(context);
-              },
-              child: const Text('Add'),
-            ),
-          ],
-        );
-      },
     );
   }
 }
